1faa351311d3212da0a56529f78f6522afd41fad
so1so2so <1037930435@qq.com>
2018-03-27 23:19:37 +0800

    Initial commit

---
+++ crm/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+import models
+
+# Register your models here.
+admin.site.register(models.Customer)
+admin.site.register(models.Branch)
+admin.site.register(models.ClassList)
